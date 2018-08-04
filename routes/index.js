4813689591fc3f8f6e7fd4d83b49212a4012f244
Carolina Amarillo <caro_amarillo@hotmail.com>
2018-08-04 12:26:28 -0300

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var bodyParser = require('body-parser');
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
 
-var urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+let users = [];
 
-/* GET home page. */
+// Home
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Express',
@@ -13,18 +14,45 @@ router.get('/', function (req, res, next) {
   });
 });
 
-let users = [];
-
-router.get('/user/list', function (req, res, next) {
-  console.log('GET ->', req.query);
-  res.render('userForm', {queryString: req.query})
-})
-
+// Create user
 router.get('/user/form', function (req, res, next) {
   res.render('userForm')
 })
 
-router.post('/user/list', urlencodedParser, function (req, res, next) {
+// User list
+router.get('/user/list', function (req, res, next) {
+
+  console.log('GET ->', req.query);
+  /*const list = document.querySelector('ul')
+
+  const searchBar = document
+    .forms['searchUsers']
+    .querySelector('input');
+  searchBar.addEventListener('keyup', function (e) {
+    const term = e
+      .target
+      .value
+      .toLowerCase();
+    const usersList = list.getElementsByTagName('li');
+    Array
+      .from(users)
+      .forEach(function (u) {
+        const title = u.firstElementChild.textContent;
+        console.log(title);
+        if (title.toLowerCase().indexOf(term) != -1) {
+          u.style.display = 'block';
+        } else {
+          u.style.display = 'none'
+        }
+      })
+  })*/
+
+  res.render('userList', {user: users})
+
+})
+
+// Create user and list
+router.post('/user/create', urlencodedParser, function (req, res, next) {
   let name = req.body.name;
   let lastname = req.body.lastname;
   let phone = req.body.phone;
@@ -40,13 +68,15 @@ router.post('/user/list', urlencodedParser, function (req, res, next) {
   users.push(userData);
   console.log('POST ->', req.body);
   console.log('POST ->', users);
-  res.render('userList', {user: users});
+  res.render('userForm', {user: users});
 })
 
+// Go to ping and render Pong =P
 router.get('/ping', function (req, res, next) {
   res.render('ping', {pong: 'pong'})
 })
 
+// Delete user
 router.get('/delete', function (req, res, next) {
 
   users = users.filter(function (todo) {
